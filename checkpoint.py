d71e1ce6cac6505f0877a191803dd197e20a8581
Yiliiii <72687714+YiandLi@users.noreply.github.com>
2022-07-05 15:54:13 +0800

    Update checkpoint.py

--- checkpoint.py
+++ checkpoint.py
@@ -54,10 +54,10 @@ def latest_checkpoint(path):
 
 
 def save(state, path, max_to_keep=None):
-    '''
+    """
     调用该方法，保存 state
     max_to_keep 为最多保存多少个ckpt
-    '''
+    """
     checkpoints = glob.glob(os.path.join(path, "*.pt"))
 
     if not checkpoints:
@@ -73,3 +73,39 @@ def save(state, path, max_to_keep=None):
     checkpoint = os.path.join(path, "model-%d.pt" % counter)
     print("Saving checkpoint: %s" % checkpoint)
     torch.save(state, checkpoint)
+    
+def save_checkpoint(step, epoch, model, optimizer, params):
+    """
+    存的是 step, epoch, model，optimizer
+    可以按照实际情况进行修改
+    """
+    if dist.get_rank() == 0:
+        state = {
+            "step": step,
+            "epoch": epoch,
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict()
+        }
+        utils.save(state, params.output, params.keep_checkpoint_max)
+        
+def load_checkpoint():
+    checkpoint = utils.latest_checkpoint(params.output)
+
+    if args.checkpoint is not None:
+        # 超参数传入的 checkpoint
+        state = torch.load(args.checkpoint, map_location="cpu")
+        model.load_state_dict(state["model"])
+        step = params.initial_step
+        epoch = 0  # 默认从 0 开始
+    elif checkpoint is not None:
+        # 默认地址的 checkpoints
+        state = torch.load(checkpoint, map_location="cpu")
+        step = state["step"]  # 保存以前没训练完的步数
+        epoch = state["epoch"]
+        model.load_state_dict(state["model"])
+
+        if "optimizer" in state:
+            optimizer.load_state_dict(state["optimizer"])
+    else:
+        step = 0  #
+        epoch = 0  # 真实 epoch 数量
